aa0ff254c959eaf02b69d38c486a4b1620fac620
Nikita Shuvalov <nshuvalov89@gmail.com>
2017-07-07 01:22:08 -0400

    Added methods to remove non-word characters from strings in order to make comparisions without having to do insane variable initialization.

--- src/sample/HandParser.java
+++ src/sample/HandParser.java
@@ -1,10 +1,31 @@
 package sample;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class HandParser {
 
-    //ToDo: To get the playerAction I should take 2nd to last position String since ACR allows spaces in names
+    private static ArrayList<String> mDealtPlayers = new ArrayList<>();
+    private static int mButtonPosition = 0;
+    private static int mActivePlayers = 0;
+    private static int mActingPlayerIndex = 0;
+    public static final String FOLDS = "folds";
+    public static final String CALLS = "calls";
+    public static final String RAISES = "raises";
+    public static final String CHECKS = "checks";
+
+    public static final String GAME = "Game";
+    public static final String ID = "ID";
+    public static final String PLAYER = "Player";
+    public static final String SEAT = "Seat";
+
+    public static final String RECEIVED = "received";
+
+    public static final String FLOP = "FLOP";
+    public static final String TURN = "TURN";
+    public static final String RIVER = "RIVER";
+
+    public static final String SUMMARY = "Summary";
 
     /**
      * Pass a handHistory file to this in order to parse the data into something meaningful.
@@ -12,39 +33,19 @@ public class HandParser {
      * @throws IOException
      */
     public static void parseHandHistory(File handHistory) throws IOException {
-        TempDataStorage tempDataStorage = TempDataStorage.getInstance();
-        String seatString = "", playerString="", receivedString="";
-        String[] gameIDStrings = new String[2];
-        BufferedReader br = new BufferedReader(new FileReader(handHistory));
         int hand= 0;
-        int count = 0;
-        boolean newHand = true;
-        boolean handsDealt = false;
-        int buttonPosition = -1;
         int blindCount = 0;
         int street = 0;
-        int activePlayers = 0;
+        boolean newHand = true;
+        boolean handsDealt = false;
+
         String lastTracked = "";
+        TempDataStorage tempDataStorage = TempDataStorage.getInstance();
+        BufferedReader br = new BufferedReader(new FileReader(handHistory));
+
 
         for(String line = br.readLine(); line!=null; line=br.readLine()){
-            String[] words = line.trim().split("\\s+");
-
-            //Put this here because for some reason I can't hardcode "Game" and "ID:" because the output is different somehow
-            if(count==2){
-                gameIDStrings[0] = words[0];
-                gameIDStrings[1] = words[1];
-            }
-
-            //Get the word "Seat" for comparing, cause the file for some reason has weird invisible spaces in it
-            if(count==4){
-                seatString = words[0];
-            }
-
-            //Get the word "Player" for comparing, cause the file for some reason has weird invisible spaces in it
-            if(playerString.isEmpty() && count>4 && !words[0].equals(seatString)){
-                playerString = words[0];
-            }
-            count++; //Just for initializing stuff
+            String[] words = line.trim().split("\\s");
 
             //Skips empty lines which are showing up for some reason.
             if(words.length==1){
@@ -52,53 +53,50 @@ public class HandParser {
             }
 
             //If "Game ID:" it's a new hand, reset all values and increment hand
-            if(words[0].equals(gameIDStrings[0]) && words[1].equals(gameIDStrings[1])){
+            if(words[0].replaceAll("\\W","").equals(GAME) && words[1].replaceAll("\\W","").equals(ID)){
                 hand++;
                 newHand = true;
                 blindCount=0;
                 lastTracked = "";
                 handsDealt = false;
                 street =0;
-                activePlayers = 0;
+                mActivePlayers = 0;
+                mDealtPlayers.clear();
             }
 
             //Move to the seating/button arrangement of table.
-            if(seatString.equals(words[0])){
+            if(words[0].replaceAll("\\W","").equals(SEAT)){
                 if(newHand){ //First iteration gives us the button position
-                    buttonPosition = Integer.valueOf(words[1].trim());
-                    System.out.println("Button: " + buttonPosition);
+                    mButtonPosition = Integer.valueOf(words[1].trim());
                     newHand = false;
                 }else{
                     //Store seat numbers and stacks?
                 }
             }
 
-            if(handsDealt && words[0].equals(playerString)){
+            if(handsDealt && words[0].replaceAll("\\W","").equals(PLAYER)){
                 switch (street){
                     case 0:
                         parsePreflopLine(line);
                         break;
                 }
             } else if (handsDealt &&
-                    (line.contains("*** FLOP ***") ||
-                            line.contains("*** TURN ***") ||
-                            line.contains("*** RIVER ***"))){
+                    (line.replaceAll("\\W","").contains(FLOP) ||
+                            line.replaceAll("\\W","").contains(TURN) ||
+                            line.replaceAll("\\W","").contains(RIVER))){
                 street++;
-            } else if (line.equals("------ Summary ------")){
-                System.out.println("Summary");
+            } else if (line.replaceAll("\\W","").equals(SUMMARY)){
+                System.out.println("---Summary---");
             }
 
             //Move to blinds-posting and hand-dealing
-            if(words[0].equals(playerString) && !handsDealt){
+            if(words[0].replaceAll("\\W","").equals(PLAYER) && !handsDealt){
                 if(blindCount<2) { //First and second iteration are blinds posted. We'll ignore these for now.
                     blindCount++;
                 }else{
                     String username = "";
                     for(int i =1; i< words.length-3; i++){ //Get the string between "Player" and "Received" as the username.
                         username+=words[i]+" ";
-                        if(receivedString.isEmpty()){
-                            receivedString = words[words.length-3]; //Sets the pseudo-constant "Received" because, again, invisible characters.
-                        }
                     }
 
                     //Ignores duplicate lines for when each player gets a card.
@@ -106,12 +104,14 @@ public class HandParser {
                         //Gonna need to create a queue/stack/list/array to keep track of players remaining in hand, in order to keep track of action.
                         lastTracked= username.trim();
                         System.out.println(username);
-                        activePlayers++;
+                        mActivePlayers++;
+                        mDealtPlayers.add(lastTracked);
                     }
 
                     //If the 3rd to last string isn't "Received"(magically enough this works for hero as well) then we've moved on to the preflop action.
-                    if(!words[words.length-3].equals(receivedString)){
+                    if(!words[words.length-3].replaceAll("\\W","").equals(RECEIVED)){
                         handsDealt=true;
+                        mActingPlayerIndex = (mButtonPosition+1)%mActivePlayers;
                         parsePreflopLine(line);
                     }
                 }
@@ -120,8 +120,18 @@ public class HandParser {
         System.out.println("Hands: " +hand);
     }
 
+    //Use f, r, c to differentiate between Fold, Raise and Check/Call. If check/call check length.
     private static void parsePreflopLine(String line){
-        System.out.println(line);
+        //Need to find a reliable way of getting the acting player's name. Might require mapping out the hand entirely.
+
+
+//        String[] words = line.split(" ");
+//        String actingPlayer = mDealtPlayers.get(mActingPlayerIndex);
+//        int actionIndex = actingPlayer.split(" ").length+1;
+//        String action = words[actionIndex].replaceAll("\\W","");
+//
+//
+//        mActingPlayerIndex = (mActingPlayerIndex+1) % mActivePlayers;
     }
 
 }
